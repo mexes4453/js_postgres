af5da485d524206b0851efad3fc3f8d78c6df351
Chiemezie Albert Udoh <Udoh@nbb.de>
2025-07-30 15:32:27 +0200

    promise alternative with async-wait

--- index.js
+++ index.js
@@ -11,23 +11,72 @@ const  client = new Client({
     database: "",
 });
 
+/*>
+ * ----------------
+ * Query: 1 - Fetch
+ * ----------------
+ * This query fetches the complete table (public.baecat_baecatuser)
+ * command to fetch query :  .then( ()=> client.query(queryTxt))
+ * -- */
+//const queryTxt = "select * from public.baecat_baecatuser";
+
+/*>
+ * ----------------
+ * Query: 2 - Fetch
+ * ----------------
+ * This query fetches the from table (public.baecat_baecatuser) an entry with id = 1
+ * Note that the same query can be written as below.
+ * const queryTxt = "select * from public.baecat_baecatuser where id = 1";
+ * but this approach is prone to sql injection by hackers.
+ * command to fetch query :  .then( ()=> client.query(queryTxt, ["1"]))
+ * --*/
+const queryTxt = "select * from public.baecat_baecatuser where id = $1";
+
 
 /*>
  * Call the method "connect" to establish connection with server that host
  * postgres. 
  * Note that this methods returns a promise and subsequent command are 
  * executed by chaining them with method 'then' */
+/*
 client.connect()
 .then( ()=> console.log("Connected successfully..."))
-/*>
- * This query fetches the complete table (public.baecat_baecatuser)
- * --
- *.then( ()=> client.query("select * from public.baecat_baecatuser")) */
-
-/*>
- * This query fetches the from table (public.baecat_baecatuser) an entry with id = 1
- * --*/
-.then( ()=> client.query("select * from public.baecat_baecatuser where id = $1", ["1"]))
+.then( ()=> client.query(queryTxt, ["1"]))
 .then( (result)=> console.table(result.rows))
 .catch( (e)=> console.log(e))
-.finally( ()=>client.end())
+.finally( ()=>client.end()) */
+/*>
+ * Alternatively, the above command can be performed in an async fashion.
+ */
+async function PG_Query(qTxt, qValue)
+{
+    try
+    {
+       await client.connect()
+       console.log("Connected successfully...")
+       const result = await client.query(qTxt, qValue);
+       console.table(result.rows)
+    }
+    catch(e)
+    {
+       console.log("Error!", e)
+    }
+    finally()
+    {
+       await client.end()
+       console.log("Client disconnected successfully.")
+    }
+}
+PQ_Query(queryTxt, ["1"]);
+
+
+/* Not used */
+class PgQuery
+{
+    constructor(n, q, v)
+    {
+       this.name = n;
+       this.query = q;
+       this.value = v;
+    }
+}
